2f8ce3c600cf2882881078d054a2d5321edcd404
ashiqis <crazyashiq7@gmail.com>
2023-03-24 16:29:08 +0300

    .

--- speech_utils.py
+++ speech_utils.py
@@ -39,6 +39,6 @@ def text_to_speech(text):
     print('tts-ing...')
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[2].id)
+    engine.setProperty('voice', voices[1].id)
     engine.say(text)
     engine.runAndWait()
