106eae39e16de15e9303fdd8754c739568491844
Jery <jery99961@gmail.com>
2023-03-24 18:15:42 +0530

    added OpenAI API key and fixed whisper api!!

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+import openai
 import speech_utils as su
 
-language = ['en-IN', 'ta-IN']
-lang = language[0]
-api=["google", "whisper", "sphinx"]
 
-print("Hello!!")
-su.text_to_speech(su.speech_to_text(api[0]))
+def main():
+    language = ['en-IN', 'ta-IN']
+    lang = language[0]
+    api = ["google", "whisper", "sphinx"]
+
+    
+    print("Hello!!")
+    su.text_to_speech(su.speech_to_text(api[1]))
+
+
+if __name__ == "__main__":
+    main()
+    # audio = open("temp.wav", "rb")
+    # output = openai.Audio.transcribe('whisper-1', audio)
+    # print(output)
--- speech_utils.py
+++ speech_utils.py
@@ -1,3 +1,8 @@
+import io
+import os
+import app as MainApp
+from time import sleep
+import openai
 import pyttsx3
 from pydub import AudioSegment
 from pydub.playback import play
@@ -5,9 +10,10 @@ import speech_recognition as sr
 
 language = ['en-IN', 'ta-IN']
 lang = language[0]
-model=["google", "whisper", "sphinx"]
+model = ["google", "whisper", "sphinx"]
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-    
+
 def speech_to_text(model=model[0]):
     r = sr.Recognizer()
 
@@ -21,24 +27,35 @@ def speech_to_text(model=model[0]):
     try:
         if model == "google":
             stt_output = r.recognize_google(audio)
+
         elif model == "whisper":
-            stt_output = r.recognize_whisper(audio, model='tiny')
+            # save audio as WAV file
+            with open("temp.wav", "wb") as f:
+                f.write(audio.get_wav_data())
+            with open("temp.wav", "rb") as audio:
+                stt_output = openai.Audio.transcribe('whisper-1', audio)
+                stt_output = stt_output['text']
+            os.remove("temp.wav")
+
         elif model == "sphinx":
             stt_output = r.recognize_sphinx(audio)
-            
-        print('Converting audio transcripts into text\n\n「'+ stt_output+'」\n')
+
+        print('Converting audio transcripts into text\n\n「' + stt_output+'」\n')
     except Exception as e:
-        stt_output="Sorry, I didnt get that.."
+        stt_output = "Sorry, I didnt get that.."
         print("\n"+stt_output+"\n", e)
 
     return stt_output
-
 
 
 def text_to_speech(text):
     print('tts-ing...')
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[2].id)
+    engine.setProperty('voice', voices[1].id)
     engine.say(text)
     engine.runAndWait()
+
+
+if __name__ == "__main__":
+    MainApp.main()
